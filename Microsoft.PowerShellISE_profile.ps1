50dade5d4e737c4227f648bc045fc6cab0a1bdab
gordonbondon <koolgen@gmail.com>
2015-07-17 14:29:08 +0300

    revert

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,5 +1,1 @@
 Set-Location D:\Dropbox\Work\Scripts
-
-Import-Module PSReadline -ErrorAction SilentlyContinue
-Set-PSReadlineKeyHandler -Key UpArrow   -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
