93706c84d10c59590795d34ed4dcac155144a3f6
Artem Iarmoliuk <gordonbondon@users.noreply.github.com>
2015-08-05 18:28:44 +0300

    Import ISEgit

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,1 +1,9 @@
+#=================================================
+#Install and configure ISEGit
+#=================================================
+if (!(Get-Module -ListAvailable | ? { $_.name -like 'ISEGit' })) {
+        Powershellget\Install-Module ISEGit
+    }
+Import-Module ISEGit
+
 Set-Location D:\Dropbox\Work\Scripts
