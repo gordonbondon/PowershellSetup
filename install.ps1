629e5c39615127270d6a6e0da220ff1c27f99f37
Artem Iarmoliuk <koolgen@gmail.com>
2016-04-01 12:44:12 +0300

    Add registri setting to enable console prompting

--- install.ps1
+++ install.ps1
@@ -18,4 +18,7 @@ foreach ($name in $profiles)
 #Add my module folder to module path via http://tomtalks.uk/2013/06/powershell-add-a-persistent-module-path-to-envpsmodulepath/
 $originalpaths = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PSModulePath).PSModulePath
 $newPath = $originalpaths + ';' + (Get-Item ..\..\Scripts\Modules\).FullName
-Set-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PS ModulePath –Value $newPath
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PSModulePath -Value $newPath
+
+#Set Get-Credential password promt co console mode
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name ConsolePrompting -Value $true
