8d477dad179e739d68b9fa32cb8be461070d2af6
Artem Iarmoliuk <koolgen@gmail.com>
2016-06-29 17:15:35 +0300

    Import new ProjectExplorer mlodule in ISE

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,3 +1,6 @@
 #Source common settings
 $dir = Split-Path $MyInvocation.MyCommand.Definition
 . "$dir\Microsoft.PowerShell_profile.Common.ps1"
+
+#Import ISE modules
+Import-Module PsISEProjectExplorer
